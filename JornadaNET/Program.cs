92faf196a2c8c1777246c9119e32cb45faa08104
George Wanderson <georgewanderson07@gmail.com>
2023-05-16 23:12:52 -0300

    Add project files.

---
+++ JornadaNET/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace JornadaNET
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
